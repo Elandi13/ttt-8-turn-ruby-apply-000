98a8e096e77118e6b530b74cec39c703c506d790
Elvis Landi <elandi13@yahoo.com>
2020-09-26 15:23:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,4 +16,8 @@ end
 def valid_move?(board, index)
   def position_taken?(array,ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return FALSE
+    else
+      return TRUE
+      
     
