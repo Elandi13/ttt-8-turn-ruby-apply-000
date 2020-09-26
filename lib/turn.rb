2ba6465c490558c7aa6961e305c1412dfe3fa461
Elvis Landi <elandi13@yahoo.com>
2020-09-26 15:18:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,5 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[2]} | #{board[3]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts 
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  
